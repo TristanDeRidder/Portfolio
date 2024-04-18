e49ad6847172539fe59babb732457397c77e8e10
pgm-tristanderidder <tristan.deridder@student.arteveldehs.be>
2024-04-18 08:35:32 +0200

    eleventy setup completed

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./input/assets');
+
+
+    return {
+        dir: {
+            input: 'input',
+            output: '_site',
+        }
+    }
+}
